05803a9b8283bd863594b17462ef62e6dc37645b
Aureen <yukiansff@gmail.com>
2020-09-08 15:29:42 +0200

    Fixed the header's responsiveness

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+//Display the collapsed navbar after the user scrolls.
 function changeNav() {
+
+  if (window.matchMedia("(max-width: 1200px)").matches) return;
 
   const navbar = document.querySelector(".navbar");
   const nav = document.querySelector(".header_expanded");
   const main = document.querySelector("main");
+  const trending = document.querySelector(".row__trending");
 
-  if (pageYOffset == 0) {
-    nav.classList.remove("d-none");
-    navbar.classList.add("d-none");
+
+  if (pageYOffset > nav.clientHeight - trending.clientHeight) {
+    navbar.classList.remove("d-lg-none");
   }
 
-  else {
-    nav.classList.add("d-none");
-    navbar.classList.remove("d-none");
+  if (pageYOffset == 0) {
+    navbar.classList.add("d-lg-none");
   }
 
 }
